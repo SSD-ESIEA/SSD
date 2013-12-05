dd6c7f9195c8f912210cc129e260a7e297628f31
mlpo <mlpo@cecmu.org>
2013-12-05 23:00:34 +0100

    Ajout des fichiers

--- site/template/main.php
+++ site/template/main.php
@@ -1,24 +1,24 @@
 <?php
 
-	function generateTemplate($nameCaller = null)
-	{
-		if($nameCaller == null)
-			return;
+function generateTemplate($nameCaller = null)
+{
+    if($nameCaller == null)
+        return;
 
-		echo '<!DOCTYPE html><html><body>';
+    echo '<!DOCTYPE html><html><body>';
 
-		include('header.php');
-		include('footer.php');
+    include('header.php');
+    include('footer.php');
 
-		if(file_exists($nameCaller . '.template.php'))
-		{
-			include($nameCaller . '.template.php');
-		}
-		else
-		{
-			include('notFound.php');
-		}
+    if(file_exists($nameCaller . '.template.php'))
+    {
+        include($nameCaller . '.template.php');
+    }
+    else
+    {
+        include('notFound.php');
+    }
 
-		echo '</body></html>';
-	}
+    echo '</body></html>';
+}
 ?>
