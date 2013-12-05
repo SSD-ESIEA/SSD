33e51afec20bf7600a76051c335033005e23f3ef
p1rox <p1rox@p1rox.fr>
2013-12-05 20:48:43 +0100

    Add inscriptionTemplate.php

---
+++ site/template/inscriptionTemplate.php
@@ -0,0 +1,8 @@
+<form method="post" target="todo.php">
+    <input type="text" placeholder="Pseudo" name="pseudo" id="pseudo">
+    <input type="password" placeholder="Password" name="password" id="password">
+    <input type="password" placeholder="Password (confirmation)" name="passeword-confirm" id="password-confirm">
+    <input type="text" placeholder="Email" name="email" id="email">
+    <input type="text" placeholder="Confirmation email" name="email-confirm" id="email-confirm">
+    <input type="submit" value="S'inscrire">
+</form>
