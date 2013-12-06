6a8b382c2ef55144ed79bbcb42ce2472650e92aa
mlpo <mlpo@cecmu.org>
2013-12-06 02:10:17 +0100

    Indentation

--- site/template/login.template.php
+++ site/template/login.template.php
@@ -1,7 +1,7 @@
 <b>Veuillez entrer vos identifiants</b><br/><br/>
 
 <form method="post" action="login.php">
-	<b>Identifiants:</b>	<input name="user" type="text">	<br/>
-	<b>Mot de passe:</b>	<input name="pass" type="password">	<br/><br/>
-	<input type="submit" value="Se connecter">
+    <b>Identifiants:</b>	<input name="user" type="text">	<br/>
+    <b>Mot de passe:</b>	<input name="pass" type="password">	<br/><br/>
+    <input type="submit" value="Se connecter">
 </form>
