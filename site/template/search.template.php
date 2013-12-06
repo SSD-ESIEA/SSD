ee50271ce2c32442ccc5e38f3e0e703d373edf04
mlpo <mlpo@cecmu.org>
2013-12-06 03:51:02 +0100

    Ajout du fichier de template

--- site/template/search.template.php
+++ site/template/search.template.php
@@ -1,5 +1,17 @@
 <?php
 
+include_once "includes/DBInterface.php"
+$bdd = new DBInterface();
 
+if(!isset($_GET['parent']))
+{
+    $objects = $bdd->getUserByParent();
+}
+else
+{
+    $objects = $bdd->getUserByParent($_GET['parent']);
+}
+
+var_dump($objects);
 
 ?>
