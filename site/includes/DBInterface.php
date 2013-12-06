4af32a75f3c61f18674d15a44e1c1b065fe043f5
mlpo <mlpo@cecmu.org>
2013-12-06 01:08:28 +0100

    Ajout de méthodes dans DBInterface

--- site/includes/DBInterface.php
+++ site/includes/DBInterface.php
@@ -4,22 +4,50 @@ include_once 'config.php';
 
 class DBInterface
 {
-        private $database;
+    private $database;
 
-        function __construct()
-        {
-            global $CONFIG;
-            $dsn = "mysql:host=" . $CONFIG['bdd_hostname'] . "; dbname=" . $CONFIG['bdd_dbname'] . ";";
-            $this->database = new PDO($dsn, $CONFIG['bdd_user'], $CONFIG['bdd_password']);
-        }
+    function __construct()
+    {
+        global $CONFIG;
+        $dsn = "mysql:host=" . $CONFIG['bdd_hostname'] . "; dbname=" . $CONFIG['bdd_dbname'] . ";";
+        $this->database = new PDO($dsn, $CONFIG['bdd_user'], $CONFIG['bdd_password']);
+    }
 
-        function isUserValid($username = NULL, $password = NULL)
-        {
-            $req = $this->database->prepare('SELECT * FROM feedme_users WHERE username = ? AND password = ?');
-            $req->execute(array($username, hash('whirlpool', $password)));
+    function isUserValid($username = NULL, $password = NULL)
+    {
+        $req = $this->database->prepare('SELECT * FROM feedme_users WHERE username = ? AND password = ?');
+        $req->execute(array($username, hash('whirlpool', $password)));
 
-            return !($req->fetch() == NULL);
-        }
+        return !($req->fetch() == NULL);
+    }
+
+    function isUserExist($username = NULL)
+    {
+        $req = $this->database->prepare('SELECT * FROM feedme_users WHERE username = ?');
+        $req->execute(array($username));
+
+        return !($req->fetch() == NULL);
+    }
+
+    function isMailAlreadyTaken($mail = NULL)
+    {
+        $req = $this->database->prepare('SELECT * FROM feedme_users WHERE mail = ?');
+        $req->execute(array($mail));
+
+        return !($req->fetch() == NULL);
+    }
+
+    function addUser($username = NULL, $password = NULL, $mail = NULL)
+    {
+        $req = $this->database->prepare('INSERT INTO feedme_users(username, password, mail) VALUES(:username, :password, :mail)');
+        $req->execute(array(
+            'username' => $username,
+            'password' => hash('whirlpool', $password),
+            'mail' => $mail,
+        ));
+
+    }
+
 }
 
 ?>
