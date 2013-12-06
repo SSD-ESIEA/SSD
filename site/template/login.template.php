51fe6561d669fdb48cd546cf9dc0e85b50a12482
Taiki <taiki@rakshata.com>
2013-12-06 02:04:46 +0100

    Stuffs

--- site/template/login.template.php
+++ site/template/login.template.php
@@ -1,6 +1,6 @@
 <b>Veuillez entrer vos identifiants</b><br/><br/>
 
-<form>
+<form method="post" action="login.php">
 	<b>Identifiants:</b>	<input name="user" type="text">	<br/>
 	<b>Mot de passe:</b>	<input name="pass" type="password">	<br/><br/>
 	<input type="submit" value="Se connecter">
