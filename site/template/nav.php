fd00d8128a13f258138b299a60ec08308989a71f
p1rox <p1rox@p1rox.fr>
2013-12-06 04:23:06 +0100

    Nav modifs

--- site/template/nav.php
+++ site/template/nav.php
@@ -1,8 +1,13 @@
 <nav>
-<a href="">item</a>
-<a href="">item</a>
-<a href="">item</a>
-<a href="">item</a>
-<a href="">item</a>
-<a href="">item</a>
+<a href="/index.php">Index</a>
+
+<?php 
+   if(isset($_SESSION['login']))
+   {
+        echo "<a href=\"/inscription.php\">Inscription</a>"
+   }
+
+?>
+
+<a href="/about.php">A propos</a>
 </nav>
