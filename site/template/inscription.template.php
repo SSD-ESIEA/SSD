88550441f444c184dcc0f261dd37fd749262d81d
Taiki <taiki@rakshata.com>
2013-12-06 04:52:49 +0100

    Add data to the inscription form

--- site/template/inscription.template.php
+++ site/template/inscription.template.php
@@ -4,5 +4,9 @@
     <input type="password" placeholder="Password (confirmation)" name="password-confirm" id="password-confirm"><br>
     <input type="text" placeholder="Email" name="email" id="email"><br>
     <input type="text" placeholder="Confirmation email" name="email-confirm" id="email-confirm"><br>
+    <input type="text" placeholder="Genre" name="email-confirm" id="email-confirm">
+    <input type="text" placeholder="Age" name="age"><br>
+	Genre: <select name="sex"><option value="0"> - </option><option value="1">Homme</option><option value="2">Femme</option></select><br>
+    <input type="text" placeholder="Ville" name="city"><br>
     <input type="submit" value="S'inscrire" name="submit">
 </form>
---
+++ site/template/inscriptionMissingData.template.php
@@ -0,0 +1,5 @@
+<font color="red"><b>Des infos sont manquantes !</b></font><br/>
+
+<?php
+	include('inscription.template.php');
+?>
